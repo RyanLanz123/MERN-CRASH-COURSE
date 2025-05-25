ec1db8a8fb8f646e0ac0ea31b279037b5484314e
RyanLanz123 <ryanlanz6@gmail.com>
2025-05-25 14:05:08 +0100

    add chakra and start creating pages

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Button, HStack, Container, Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FaPlusSquare } from 'react-icons/fa';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { useColorMode } from './ui/color-mode';
+import { LuSun } from 'react-icons/lu';
+import { IoMoon } from 'react-icons/io5';
+
+
+const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();  
+  return (
+    <Container maxW="1140px" px={4}>
+      <Flex
+        h={16}
+        alignItems="center"
+        justifyContent="space-between"
+        flexDir={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text
+            fontSize={{ base: '22px', sm: '28px' }}
+            fontWeight="bold"
+            textTransform="uppercase"
+            textAlign="center"
+            bgGradient="linear(to-r, cyan.400, blue.500)"
+            bgClip="text"
+        >
+            <ChakraLink as={RouterLink} to="/">
+                Product Store
+            </ChakraLink>
+        </Text>
+
+        <HStack spacing={2} alignItems="center">
+          <Link to="/create">
+            <Button>
+              <FaPlusSquare fontSize={20} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode == "light" ? <IoMoon /> :
+               <LuSun size ="20" /> }
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <main>
+      <h2>Welcome to the Home Page</h2>
+      {/* Add more content here */}
+    </main>
+  );
+};
+
+export default HomePage;
